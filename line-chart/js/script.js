12a110e61e8ed8ed071e3a7aac4f1d9312b6a0c5
OGladfelter <ogladfelter@gmail.com>
2024-04-01 15:01:16 -0500

    add toggle

--- line-chart/js/script.js
+++ line-chart/js/script.js
@@ -3,9 +3,6 @@ const heightAdjuster = .8;
 
 function lineChart() {
 
-
-    console.log('test');
-
     // set the dimensions and margins of the graph
     const margin = {top: 10, right: 30, bottom: 30, left: 60}
 
@@ -66,8 +63,82 @@ function lineChart() {
     })
 }
 
+function lineChart2() {
+
+    // set the dimensions and margins of the graph
+    const margin = {top: 10, right: 30, bottom: 30, left: 60}
+
+    let width, height;
+
+    if (mobile) {
+        width = window.innerWidth * 0.9 - margin.left - margin.right;
+    } else {
+        width = window.innerWidth * 0.5 - margin.left - margin.right;
+    }
+
+    height = 400 - margin.top - margin.bottom;
+
+    // append the svg object to the body of the page
+    const svg = d3.select("#lineChart2")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    //Read the data
+    d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/3_TwoNumOrdered_comma.csv",
+
+    // When reading the csv, I must format variables:
+    function(d){
+        return { date : d3.timeParse("%Y-%m-%d")(d.date), value : d.value }
+        }).then(
+
+            // Now I can use this dataset:
+            function(data) {
+
+            // Add X axis --> it is a date format
+            const x = d3.scaleTime()
+            .domain(d3.extent(data, function(d) { return d.date; }))
+            .range([ 0, width ]);
+            svg.append("g")
+            .attr("transform", `translate(0, ${height})`)
+            .call(d3.axisBottom(x));
+
+            // Add Y axis
+            const y = d3.scaleLinear()
+            .domain([0, d3.max(data, function(d) { return +d.value; })])
+            .range([ height, 0 ]);
+            svg.append("g")
+            .call(d3.axisLeft(y));
+
+            // Add the line
+            svg.append("path")
+            .datum(data)
+            .attr("fill", "none")
+            .attr("stroke", "red")
+            .attr("stroke-width", 1.5)
+            .attr("d", d3.line()
+                .x(function(d) { return x(d.date) })
+                .y(function(d) { return y(d.value) })
+                )
+    })
+}
+
+function switchChart() {
+    const lineChart1DisplayStatus = document.getElementById("lineChart").style.display;
+    if (lineChart1DisplayStatus == 'block') {
+        document.getElementById("lineChart2").style.display = 'block';
+        document.getElementById("lineChart").style.display = 'none';
+    } else {
+        document.getElementById("lineChart2").style.display = 'none';
+        document.getElementById("lineChart").style.display = 'block';
+    }
+}
+
 function main() {
     lineChart();
+    lineChart2();
 }
 
 main();
